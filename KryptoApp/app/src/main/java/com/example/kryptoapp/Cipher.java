c8f3b13b5e4396c853a1bfe1ad400331084ac8ca
Ethan Otele <61558929+EthanOtele@users.noreply.github.com>
2020-03-04 11:58:12 -0500

    Update Cipher.java
    Added comments

--- KryptoApp/app/src/main/java/com/example/kryptoapp/Cipher.java
+++ KryptoApp/app/src/main/java/com/example/kryptoapp/Cipher.java
@@ -1,20 +1,22 @@
 package com.example.kryptoapp;
 
-import android.widget.Toast;
+
 
 public class Cipher {
-
+    // Provided by the Implementation section of the Lab
+    // Input variable key
+    // If your code prints out B's where a whitespace should be, place a whitespace in front of the ALPHABET
     private String key;
     public static final String ALPHABET = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    // Default constructor
     public Cipher(String k)
     {
             this.key = k;
     }
 
-
-
-
+    // Provided by the textbook
+    // Responsible for assigning each character in String note with a value
     public String makePad(String note)
     {
         String pad;
@@ -22,12 +24,16 @@ public class Cipher {
         return pad;
     }
 
-
-
+    // Provided by the textbook
     public String encrypt(String note)
     {
         String pad = makePad(note);
         String result = "";
+        // For loop that iterates over each character in String note.
+        // position variable grabs the characters index value from the alphabet
+        // shift variable is how much the character will move forward within the alphabet, uses the value assigned in makePad
+        // newPosition variable is the new index of the original character
+        // result variable uses the new index to grab a character from alphabet and adds it to the result string
         for (int i = 0; i < note.length(); i++)
         {
             String c = note.substring(i, i + 1);
@@ -36,17 +42,20 @@ public class Cipher {
             int newPosition = (position + shift) % ALPHABET.length();
             result = result + ALPHABET.substring(newPosition, newPosition + 1);
         }
-
         return result;
     }
 
-
-
+    // Same code as encrypt, but it is used to decrypt an encrypted string
     public String decrypt(String note)
     {
         String pad = makePad(note);
         String result = "";
         for (int i = 0; i < note.length(); i++)
+        // For loop that iterates over each character in String note.
+        // position variable grabs the characters index value from the alphabet
+        // shift variable is how much the character will move backward within the alphabet, uses the value assigned in makePad
+        // newPosition variable is the new index of the original character, Math.abs is used to prevent negatives, will crash if you don't use it
+        // result variable uses the new index to grab a character from alphabet and adds it to the result string
         {
             String c = note.substring(i, i + 1);
             int position = ALPHABET.indexOf(c);
